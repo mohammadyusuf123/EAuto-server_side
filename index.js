849a6068a9ecf63c21cf658a081261a757827fbe
Zayn Yusuf <yusue786@gmail.com>
2022-05-26 14:41:09 +0600

    privte-admin-route

--- index.js
+++ index.js
@@ -52,17 +52,30 @@ app.get('/user',verifyJWT,async(req,res)=>{
 
 })
 // Admin API
-app.put('/user/admin/:email',async(req,res)=>{
+app.get('/admin/:email',async(req,res)=>{
+  const email=req.params.email
+  const user=await usersCollection.findOne({email:email})
+  const isAdmin=user.role==='admin'
+  res.send({admin:isAdmin})
+})
+app.put('/user/admin/:email',verifyJWT,async(req,res)=>{
   const email=req.params.email;
-  const filter={email:email}
+  const requester=req.decoded.email
+  const requesterAccount=await usersCollection.findOne({email:requester})
+  if(requesterAccount.role==='admin'){
+    const filter={email:email}
  
     const updateDoc={
         $set:{role:'admin'}
            
 }
 const result=await usersCollection.updateOne(filter,updateDoc)
-
    res.send(result);
+  }
+  else{
+    res.status(403).send({message:'forbidden'})
+  }
+  
 
 })
     // User API
